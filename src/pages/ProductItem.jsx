d52cc581a342eaf5846543929c0714e84e5b908c
Arik Alexandrov <arikxl@gmail.com>
2022-02-02 23:28:21 +0200

    redux

--- src/pages/ProductItem.jsx
+++ src/pages/ProductItem.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { ProductService } from '../services/products';
+
+const ProductItemStyled = styled.section`
+    img {
+        width: 200px;
+        height: 200px;
+    }
+`;
+
 
 
 const ProductItem = () => {
     const id = useParams().id;
-
+    
     const [product, setProduct] = useState({});
-
+    
     useEffect(() => {
         ProductService.fetchProduct(id)
-            .then(data => setProduct(data))
+        .then(data => setProduct(data))
     }, [id])
-
+    
     return (
-        <div>
+        <ProductItemStyled>
             <h1>
+                id:{product._id}
+                <br />
                 {product.title}
             </h1>
+            <p>{product.price}</p>
+            <img src={product.img} alt="" />
 
             <Link to={'/'}>
                 <button>🏠</button>
             </Link>
-        </div>
+        </ProductItemStyled>
     );
 };
 

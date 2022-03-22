940d8b796138a51bb226522cea3763d98caba378
Arik Alexandrov <arikxl@gmail.com>
2022-03-22 22:36:20 +0200

    shipping address redux

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import { useState } from 'react';
+import React ,  { useState }from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
+import saveShippingAddress from '../redux/actions/cartActions';
+
 
 const Shipping = () => {
 
-    const [address, setAddress] = useState('');
-    const [city, setCity] = useState('');
-    const [zipcode, setZipcode] = useState('');
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart);
+    const {shippingAddress} = cart;
+
+    const [address, setAddress] = useState(shippingAddress.address);
+    const [city, setCity] = useState(shippingAddress.city);
+    const [zipcode, setZipcode] = useState(shippingAddress.zipcode);
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    }
+        dispatch(saveShippingAddress({ address, city, zipcode}));
+        navigate('/payment');
+    };
 
-  return (
-    <div>
-        <form onSubmit={handleSubmit} className="flex column w200">
-            <h6>delivery address</h6>
-            <input type="text" placeholder="Enter delivery address"
-            value={address} required
-             onChange={(e) => setAddress(e.target.value)}/>
-            <input type="text" placeholder="Enter city"
-            value={city} required
-            onChange={(e) => setCity(e.target.value)}/>
-            <input type="number" placeholder="Enter zipcode"
-            value={zipcode} required
-            onChange={(e) => setZipcode(e.target.value)}/>
-            <button type="submit" className="">Continue</button>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="flex column w200">
+                <h6>delivery address</h6>
+                <input type="text" placeholder="Enter delivery address"
+                    value={address} required
+                    onChange={(e) => setAddress(e.target.value)} />
+                <input type="text" placeholder="Enter city"
+                    value={city} required
+                    onChange={(e) => setCity(e.target.value)} />
+                <input type="number" placeholder="Enter zipcode"
+                    value={zipcode} required
+                    onChange={(e) => setZipcode(e.target.value)} />
+                <button type="submit" className="">Continue</button>
+            </form>
+        </div>
+    )
 }
 
 export default Shipping
--- src/redux/store.js
+++ src/redux/store.js
@@ -28,9 +28,15 @@ const userInfoFromLocalStorage = localStorage.getItem('userInfo')
 ? JSON.parse(localStorage.getItem('userInfo'))
 : null;
 
+// SHIPPING ADDRESS
+const shippingAddressFromLocalStorage = localStorage.getItem('shippingAddress')
+? JSON.parse(localStorage.getItem('shippingAddress'))
+: {};
+
 const initialState = {
     cart: {
         cartItems: cartItemsFromLocalStorage,
+        shippingAddress: shippingAddressFromLocalStorage
     },  
     userLogin : {
         userInfo: userInfoFromLocalStorage
